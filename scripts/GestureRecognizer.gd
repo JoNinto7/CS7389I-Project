47885317a2962979c3aeda2bd46340dfefa7829a
Nicky <nikhilmaramreddy@gmail.com>
2025-04-26 15:01:07 -0500

    Flicks working

---
+++ scripts/GestureRecognizer.gd
@@ -0,0 +1,50 @@
+extends Node
+
+signal flick_detected(direction: String)
+
+@export var controller_node_path: NodePath
+@export var debug_label_path: NodePath
+
+var controller: XRController3D
+var debug_label: Label3D
+
+const FLICK_SPEED_THRESHOLD := 1.5
+const COOLDOWN_TIME := 0.5
+
+var last_position: Vector3
+var cooldown_timer := 0.0
+
+func _ready():
+	controller = get_node(controller_node_path)
+	debug_label = get_node(debug_label_path)
+	last_position = controller.global_transform.origin
+
+func _physics_process(delta):
+	if not controller:
+		return
+
+	if cooldown_timer > 0:
+		cooldown_timer -= delta
+		return
+
+	var current_position = controller.global_transform.origin
+	var velocity = (current_position - last_position) / delta
+
+	last_position = current_position
+
+	if velocity.length() > FLICK_SPEED_THRESHOLD:
+		var dir = get_flick_direction(velocity)
+		if dir != "":
+			emit_signal("flick_detected", dir)
+			if debug_label:
+				debug_label.text = "Gesture: " + dir
+			cooldown_timer = COOLDOWN_TIME
+
+func get_flick_direction(velocity: Vector3) -> String:
+	if abs(velocity.x) > abs(velocity.y) and abs(velocity.x) > abs(velocity.z):
+		return "right" if velocity.x > 0 else "left"
+	elif abs(velocity.y) > abs(velocity.x) and abs(velocity.y) > abs(velocity.z):
+		return "up" if velocity.y > 0 else "down"
+	elif abs(velocity.z) > abs(velocity.x) and abs(velocity.z) > abs(velocity.y):
+		return "forward" if velocity.z < 0 else "backward"
+	return ""
